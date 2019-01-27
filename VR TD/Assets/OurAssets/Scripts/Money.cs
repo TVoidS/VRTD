be6abfc07e29da36521061b61b901e581cb45490
William Bullock <wbullock@ggc.edu>
2019-01-26 21:23:07 -0500

    Adding basic border materials and money script

---
+++ VR TD/Assets/OurAssets/Scripts/Money.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour {
+
+	public int cash = 0;
+	public int startingCash = 500;
+
+	// Use this for initialization
+	void Start () {
+		cash = startingCash;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public int getCash () {
+		return cash;
+	}
+
+	public void addCash (int amount) {
+		cash += amount;
+	}
+
+	public void removeCash (int amount) {
+		cash -= amount;
+	}
+}
